5fde821fdd395dfa1ca6f47af3152e1ffdcbc42e
yozora39020205-prog <yozora39020205@gmail.com>
2026-05-08 01:53:25 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,38 @@
-const CACHE_NAME = 'zep-prompter-v7';
-const urlsToCache = ['index.html', 'manifest.json'];
+const CACHE_NAME = 'zep-prompter-cache-v9';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
+// インストール時にファイルを強制キャッシュ
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
 });
 
+// 古いキャッシュを削除して最新版を保つ
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// オフライン時はキャッシュから返す
 self.addEventListener('fetch', (event) => {
-  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
 });
